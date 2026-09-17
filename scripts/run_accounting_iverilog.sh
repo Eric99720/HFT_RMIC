4d9a6f11aca46768f2d4e9adb238b4908be35813
Eric99720 <erichsien0720@gmail.com>
2026-09-17 22:28:42 +0800

    test: compile shared futures transition in accounting regression

--- scripts/run_accounting_iverilog.sh
+++ scripts/run_accounting_iverilog.sh
@@ -7,6 +7,7 @@ mkdir -p "$BUILD"
 iverilog -g2012 \
   -I "$ROOT/rtl/include" \
   -o "$BUILD/hft_rmic_futures_accounting_v1_tb.vvp" \
+  "$ROOT/rtl/accounting/hft_rmic_futures_transition_v1.sv" \
   "$ROOT/rtl/accounting/hft_rmic_futures_accounting_v1.sv" \
   "$ROOT/tb/tb_hft_rmic_futures_accounting_v1.sv"
 
