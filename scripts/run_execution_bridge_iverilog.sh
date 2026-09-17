03775a35be0d57f6bc8f5a0e0491c38d014dd0b0
Eric99720 <erichsien0720@gmail.com>
2026-09-17 22:29:10 +0800

    test: compile shared futures transition in execution regression

--- scripts/run_execution_bridge_iverilog.sh
+++ scripts/run_execution_bridge_iverilog.sh
@@ -10,6 +10,7 @@ iverilog -g2012 \
   -o "$BUILD/committed_execution_bridge_tb.vvp" \
   "$ROOT/tb/stubs/amu_banked_double_hash_v2_stub.sv" \
   "$ROOT/rtl/integration/hft_rmic_futures_order_store_v1.sv" \
+  "$ROOT/rtl/accounting/hft_rmic_futures_transition_v1.sv" \
   "$ROOT/rtl/accounting/hft_rmic_futures_accounting_v1.sv" \
   "$ROOT/rtl/accounting/hft_rmic_state_ram.sv" \
   "$ROOT/rtl/accounting/hft_rmic_futures_state_manager_v1.sv" \
